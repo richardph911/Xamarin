590aa23cd4e002092318e2e793baa35c4a45b7b2
richardph911 <dung.t.pham@sjsu.edu>
2020-08-17 23:21:21 -0700

    TabbedPage

---
+++ Exercise_Login/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Exercise_Login
+{
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Exercise_Login/MapPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Exercise_Login
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
